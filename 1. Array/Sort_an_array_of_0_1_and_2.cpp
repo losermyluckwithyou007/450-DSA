91fcb9730b66e4b7a30bf27fbd96eeb2a3d84511
Pawan Kumar <losermyluckwithyou007@gmail.com>
2021-06-15 12:20:52 +0530

    Sorting 0 1 2

---
+++ 1. Array/Sort_an_array_of_0_1_and_2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void Sort(int A[], int n){
+
+    int low,mid,high;
+    low=0;
+    mid=0;
+    high=n-1;
+
+    while(mid<=high){
+
+        switch(A[mid]){
+
+            case 0:
+                swap(A[low], A[mid]);
+                low++;
+                mid++;
+                break;
+
+            case 1:
+                mid++;
+                break;
+
+            case 2:
+                swap(A[mid], A[high]);
+                high--;
+                mid++;
+                break;        
+        }
+    }
+}
+
+int main(){
+    
+    int  arr[]= {0, 2, 1, 2, 0};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    Sort(arr, n);
+
+    for(int sort : arr)
+        cout<<sort<<" ";
+
+
+    return 0;
+}
