92318ffcb28ca0c1dff9ac7b2d9c947842177c04
Pawan Kumar <losermyluckwithyou007@gmail.com>
2021-06-24 17:32:11 +0530

    mIn jumps to reach end array

---
+++ 1. Array/min_No_of_jumps_to_reach_end.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int No_of_jumps(int A[], int n){
+    
+    if (A[0] <= 0)
+        return -1;
+        
+        int maxReach=A[0];
+        int jumps=1;
+        int steps=A[0];
+        
+        for(int i=1;i<n;i++){
+            
+            if(i==n-1) return jumps;
+            
+            maxReach = max(maxReach, i+A[i]);
+            
+            steps--;
+            
+            if(steps == 0 ){ jumps++;
+            
+            if(i >= maxReach) return -1;
+            
+            steps = maxReach - i;
+            
+            }
+        }
+        return -1;
+}
+
+int main(){
+
+
+    int A[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+    int  n = sizeof(A)/sizeof(A[0]);
+
+    cout<<No_of_jumps(A,n);
+
+    return 0;
+}
