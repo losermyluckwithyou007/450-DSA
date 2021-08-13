cbe376438015321922bbde98545ec9541eae5b6b
Pawan Kumar <losermyluckwithyou007@gmail.com>
2021-08-13 11:16:17 +0530

    median of arrays

--- 1. Array/Median_of_two_sorted_array_of_different_sizes.cpp
+++ 1. Array/Median_of_two_sorted_array_of_different_sizes.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int getMedian(int arr1[], int arr2[], int n){
+int getMedian(int arr1[], int arr2[], int n, int m){
 
     vector<int>v;
     int i=0;
     int j=0;
-    while(i<n && j<n){
+    while(i<n && j<m){
 
         if(arr1[i]<arr2[j]){
             v.push_back(arr1[i]);
@@ -20,7 +20,7 @@ int getMedian(int arr1[], int arr2[], int n){
     }
     
     for(;i<n;i++) v.push_back(arr1[i]);
-    for(;j<n;j++) v.push_back(arr2[j]);
+    for(;j<m;j++) v.push_back(arr2[j]);
 
     // sort( v.begin(), v.end() );
 
@@ -35,12 +35,13 @@ int getMedian(int arr1[], int arr2[], int n){
 
 int main(){
 
-    int arr1[] = {1, 2, 3, 6};
-    int arr2[] = {4, 6, 8, 10};
-    int n = sizeof(arr1)/sizeof(arr1[0]);
-    int m = sizeof(arr2)/sizeof(arr2[0]);
+    int ar1[] = {2, 3, 5, 8};
+    int ar2[] = {10, 12, 14, 16, 18, 20};
 
-    cout<<"Average of middle elements is = "<< getMedian(arr1, arr2, n);
+    int n = sizeof(ar1)/sizeof(ar1[0]);
+    int m = sizeof(ar2)/sizeof(ar2[0]);
+
+    cout<<"Median of diff sizes arrays = "<<getMedian(ar1, ar2, n, m);
 
     return 0;
 }
